09dc22da23f64b7c0a6e2487aa202576ce234b55
Josh Graber <jgraber@renovo1.com>
2015-11-03 10:05:31 -0500

    Log the message that is being queued.

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 var rabbit = require('./rabbit');
 var messageParser = require('./parser');
+var logger = require('node-queue-logger');
+
+var rabbitConfig = rabbit.config;
+logger.configure({
+	host: rabbitConfig.host,
+	login: rabbitConfig.login,
+	password: rabbitConfig.password,
+	exchange: 'LOG',
+});
 
 var message = messageParser.load();
 
@@ -8,6 +17,7 @@ var defaultRoute = '';
 var exchangeStream = rabbit.connect();
 exchangeStream.subscribe(function(exchange) {
 	console.log('- publishing message -');
+	logger.info(message);
 	exchange.publish(defaultRoute, message.body, {
 		headers: {
 			channel: message.channel,
